553850b1c68560404c5dcc01b5f2155cbd396fb3
hrishikesh2708 <hrishith27@gmail.com>
2021-04-08 22:53:19 +0530

    connecting front end and backend

--- express_backend/app.js
+++ express_backend/app.js
@@ -1,7 +1,21 @@
-const express = require('express');
-const connection = require('./config/database');
+const express = require("express");
+const mongoose = require("mongoose");
+const db = require("./config/default.json").mongoUri;
 const app = express();
-connection();
-app.get('/',(req,res) => res.send("hello world!"));
-const port = 4201;
-app.listen(port,()=> console.log(`server started on port: ${port}`));
+const passport = require("passport");
+const users = require("./routes/api/users");
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+mongoose.connect(db,{ useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("MongoDB successfully connected"))
+  .catch(err => console.log(err));
+const port = process.env.PORT || 4201;
+app.use(passport.initialize());
+require("./config/passport")(passport);
+app.use("/api/users", users);
+app.get('/', (req, res) => res.send('Hello world!')); 
+app.listen(port, () => console.log(`server started on port: ${port}`));
+
+
+
+
