fe4a24395eb8fbd147067a015ad4ae59ff72cb23
hrishikesh2708 <hrishith27@gmail.com>
2021-06-09 13:12:41 +0530

    commit

--- react_frontend/src/PrivateRoute.js
+++ react_frontend/src/PrivateRoute.js
@@ -2,18 +2,35 @@ import React from "react";
 import { Redirect, Route } from "react-router-dom";
 
 const PrivateRoute = ({ component: Component, ...rest }) => {
-  const isLoggedIn = localStorage.getItem("jwtToken") === null ? false : true;
-    // console.log(localStorage.getItem("jwtToken"))
+  let pri = "";
+  let token = localStorage.getItem("jwtToken");
+  React.useEffect(() => {
+    
+    if (token !== null) {
+      pri = true
+      console.log("if",token, pri);
+    } else {
+      pri = false
+      console.log("else",token, pri);
+    }
+  }, [pri]);
   return (
     <Route
       {...rest}
       render={(props) =>
-        isLoggedIn ? (
+        pri ? (
+          <>
+          {console.log(token, pri)}
           <Component {...props} />
+          </>
         ) : (
+          <>
+          {console.log(token, pri)}
           <Redirect
             to={{ pathname: "/login", state: { from: props.location } }}
           />
+          </>
+ 
         )
       }
     />
--- react_frontend/src/component/layout/home/NewHome.js
+++ react_frontend/src/component/layout/home/NewHome.js
@@ -336,7 +336,7 @@ export default function NewHome() {
   useEffect(() => {
     // console.log("props" , props.event)
     let eventSource = new EventSourcePolyfill(
-      `${process.env.REACT_APP_LOCALHOST}/app/#/api/stream`,
+      `${process.env.REACT_APP_LOCALHOST}/api/stream`,
       {
         headers: {
           Authorization: localStorage.getItem("jwtToken"),
